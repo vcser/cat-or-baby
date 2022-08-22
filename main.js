d8a1b2f170b70a2e0cfa204328bee6295704c67d
Vicente <vcser2018@udec.cl>
2022-08-21 21:56:27 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+let score = 0;
+let correct_answer = "ueee!"
+
+const cat_button = document.getElementById("cat-button");
+const baby_button = document.getElementById("baby-button");
+const replay_button = document.getElementById("replay");
+let audio = new Audio();
+
+window.onload = () => {
+    console.log("cargado");
+    new_round();
+}
+
+cat_button.onclick = () => {
+    console.log("cat!");
+    choose("cat");
+}
+
+baby_button.onclick = () => {
+    console.log("baby!");
+    choose("baby");
+}
+
+replay_button.onclick = () => {
+    audio.play();
+}
+
+let new_round = () => {
+    let rnd = Math.floor(Math.random() * 100) % 2;
+    if (rnd == 1) {
+        audio.src = "sounds/cat.wav";
+        correct_answer = "cat";
+    } else {
+        audio.src = "sounds/baby.wav";
+        correct_answer = "baby";
+    }
+    audio.play();
+};
+
+let choose = (selection) => {
+    if (selection == correct_answer) {
+        score++;
+    }
+    new_round();
+}
