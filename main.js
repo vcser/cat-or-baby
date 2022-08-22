f7e460a7867e77e60bbe147fb78cf9b50f97b646
Vicente <vcser2018@udec.cl>
2022-08-21 22:38:14 -0400

    still trying to fix window.onload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ window.addEventListener("load", () => {
     new_round();
 }, false);
 
-document.onmousemove = () => {
+document.onmouseenter = () => {
     audio.play();
     document.onmousemove = null;
 }
