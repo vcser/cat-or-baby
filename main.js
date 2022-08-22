1fa27754a51ce4753d2330cb6cedc22403268e78
Vicente <vcser2018@udec.cl>
2022-08-21 23:07:37 -0400

    add score

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let correct_answer = "ueee!"
 const cat_button = document.getElementById("cat-button");
 const baby_button = document.getElementById("baby-button");
 const replay_button = document.getElementById("replay");
+const score_text = document.getElementById("score");
+const result_text = document.getElementById("result");
 let audio = new Audio();
 audio.src = "sounds/cat.wav";
 
@@ -41,6 +43,18 @@ let new_round = () => {
 let choose = (selection) => {
     if (selection == correct_answer) {
         score++;
+        result_text.innerText = "Correct!";
+        change_result_color();
+    } else {
+        score = 0;
+        result_text.innerText = "Wrong!";
+        result_text.style.color = "red";
     }
+    score_text.innerText = "Score = " + score;
     new_round();
 }
+
+let change_result_color = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    result_text.style.color = randomColor;
+};
